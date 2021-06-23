0f456957a11c3f8d8abb522147998ca157540ab2
darwindel <darwindel00@gmail.com>
2021-06-23 18:45:47 -0400

    final string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-int main () { char Mystr[] = "Programming is like building a multilingual puzzle" ; puts(Mystr); return 0 ; }
+#include <stdio.h>
+
+/**
+* main - Print the string in the put function
+*
+* Return: 0 if successful
+*/
+int main(void)
+{
+Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+}
