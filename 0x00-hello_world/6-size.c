49f2db488c92f25936aaca67a4739afa10786636
darwindel <darwindel00@gmail.com>
2021-06-23 20:51:22 -0400

    added bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 */
 int main(void)
 {
-	printf("Size of a char: %lu\n", sizeof(char));
-	printf("Size of an int: %lu\n", sizeof(int));
-	printf("Size of a long int: %lu\n", sizeof(long int));
-	printf("Size of a long long int: %lu\n", sizeof(long long int));
-	printf("Size of a float: %lu\n", sizeof(float));
+	printf("Size of a char: %lu bytes\n", sizeof(char));
+	printf("Size of an int: %lu bytes\n", sizeof(int));
+	printf("Size of a long int: %lu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes\n", sizeof(float));
 	return (0);
 }
 
