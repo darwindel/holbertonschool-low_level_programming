a2e511d78bc74580a9d8d2cdf7e7a2c9ef19b9ef
darwindel <darwindel00@gmail.com>
2021-06-23 20:38:50 -0400

    printed various types of my computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - print the data types
+*
+*Return: 0 if successful
+*/
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(long int));
+	printf("%lu\n", sizeof(long long int));
+	printf("%lu\n", sizeof(float);
+	return (0);
+}
+
