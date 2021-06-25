6ddf0dd9a4a2c05deaa7e1e27fcb13b62601a6fa
darwindel <darwindel00@gmail.com>
2021-06-25 18:57:06 -0400

    print a through z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - will print the alphabet in lowercase
+*
+* Return: 0 if successful
+*/
+int main(void)
+
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z' ; ch++)
+
+{
+		putchar(ch);
+}
+	putchar('\n');
+	return (0);
+}
