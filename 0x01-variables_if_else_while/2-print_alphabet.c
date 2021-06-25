76b93a447b37ea660f162dd02cca00d6d2bd58a3
darwindel <darwindel00@gmail.com>
2021-06-25 19:03:30 -0400

    added a blank line after declarations

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
 /**
 *main - will print the alphabet in lowercase
 *
 * Return: 0 if successful
 */
 int main(void)
-
 {
 	char ch;
-	for (ch = 'a'; ch <= 'z' ; ch++)
 
+	for (ch = 'a'; ch <= 'z' ; ch++)
 {
-		putchar(ch);
+	putchar(ch);
 }
 	putchar('\n');
 	return (0);
