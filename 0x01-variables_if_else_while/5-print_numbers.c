70d663abb5093498c218e0c9c21000450ef8cb20
darwindel <darwindel00@gmail.com>
2021-06-25 22:39:42 -0400

    fixed the variable

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Will print numbers to ten starting from zero
+*
+* Return: 0 if successful
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+{
+	printf("%d", n);
+}
+	return (0);
+}
