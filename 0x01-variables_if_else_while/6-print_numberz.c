0321836c2e90ccfaa81e945c745a46a41ad229ce
darwindel <darwindel00@gmail.com>
2021-06-25 23:43:59 -0400

    fixed the new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Displaying numbers with putchar
+*
+* Return: 0 if correct"
+*/
+int main(void)
+{
+	char i;
+
+	for (  i = '0'; i <= '9'; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+	return (0);
+}
