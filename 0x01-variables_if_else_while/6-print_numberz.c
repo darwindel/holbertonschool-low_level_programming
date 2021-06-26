d6fdb978201389c7cd9ef8895c42584d0e823018
darwindel <darwindel00@gmail.com>
2021-06-26 12:41:10 -0400

    will print single digit numbers of base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,16 +2,23 @@
 /**
 * main - Displaying numbers with putchar
 *
-* Return: 0 if correct"
+* Return: 0 if correct
 */
 int main(void)
+{ 
+	int n;
+	
+	if (n < 0) 
 {
-	char i;
+	putchar('-');
+	n=-n;
+}
+	if (n / 10 != 0)
+{	
+	putchar(n / 10);
+}
 
-	for (  i = '0'; i <= '9'; i++)
-{
-	putchar(i);
-}
-	putchar('\n');
 	return (0);
+
 }
+
