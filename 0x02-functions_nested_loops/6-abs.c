4c9e03e2c6198909a8adf75c3e9e445ce4697061
darwindel <darwindel00@gmail.com>
2021-06-28 22:56:24 -0400

    added the c to int

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,12 @@
 /**
 * _abs - this will compute the absolute value of an integer
 *
+* @n: is the int that I will use to put into _abs
+*
 * Return: 0 if successful
 */
-int _abs(int)
+int _abs(int n)
 {
-	int n;
 
 	if (n < 0)
 	return (-n);
