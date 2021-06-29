d6a504467bd46b449e7eafd21bdf02dca53fc15e
darwindel <darwindel00@gmail.com>
2021-06-28 22:29:26 -0400

    will check if letter, upper or lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _isalpha - will check for letter, lowercase or uppercase
+*
+* @c: Number to pass to _islower function
+*
+* Return: 0 if successful
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}
