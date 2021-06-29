187918e9a02f3eab00c75dc8259d4429f2d9053c
darwindel <darwindel00@gmail.com>
2021-06-28 22:53:29 -0400

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _abs - this will compute the absolute value of an integer
+*
+* Return: 0 if successful
+*/
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	return (-n);
+	else
+	return (n);
+}
