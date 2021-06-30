616509937afb1e3bf3ca836de04e0a932b5482a3
darwindel <darwindel00@gmail.com>
2021-06-30 15:32:30 -0400

    took out comma

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,14 +19,14 @@ void times_table(void)
 	if (n <= 9 && n != 0)
 {
 	if (x <= 9)
-	printf("  %d,", x);
+	printf("  %d", x);
 	else
 	printf(" %d ", x);
 }
 	else
 {
 	if (n == 0)
-	printf("%d ", x);
+	printf("%d,", x);
 	else
 	printf(" %d", x);
 }
