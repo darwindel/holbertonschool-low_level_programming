fb42edafc7534b46d8291d5b45cf250eec2504da
darwindel <darwindel00@gmail.com>
2021-06-29 23:25:32 -0400

    fixed the putchar

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,15 +6,20 @@
 */
 void jack_bauer(void)
 {
-	int h;
-	int m;
+	int h = 0;
+	int m = 0;
 
 	for (h = 0; h <= 23; h++)
 {
 	for (m = 0; m <= 59; m++)
 {
+	if (h < 10)
+	_putchar('0');
 	_putchar(h);
 	_putchar(':');
+
+	if (m < 10)
+	_putchar('0');
 	_putchar(m);
 	_putchar('\n');
 }
