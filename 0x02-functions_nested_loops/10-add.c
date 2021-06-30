818afbc54d22caf5bd15d89fde9e9f3835224a18
darwindel <darwindel00@gmail.com>
2021-06-29 21:29:25 -0400

    will add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* add - this will add two integers and return the result
+*
+* @a: number will go to add prototype
+* @b: number will go to add prototype
+*
+* Return: 0 if successful
+*
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
