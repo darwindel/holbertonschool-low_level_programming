de8311d0e79bfe08594b4bca8143d717b2c8cfdc
darwindel <darwindel00@gmail.com>
2021-06-29 23:38:56 -0400

    made an adjustment to the for loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,18 +9,26 @@
 */
 void print_to_98(int n)
 {
-	int x, i;
+	int i;
 
-	if (n < 0)
-	x = 0;
+	if (n > 98)
+{
+	for (i = n; i >= 98; i--)
+{
+	if (i != 98)
+	printf("%d, ", i);
 	else
-	x = n;
-
-	for (i = x; i < 98; i++)
+	printf("%d, ", i);
+}
+}
+	else
+{
+	for (i = n; i <= 98; i++)
 {
 	if (i != 98)
 	printf("%d, ", i);
 	else
 	printf("%d", i);
+}
 }
 }
