cf73c3bbd7e7a5183c9a8e17f033f178d227e736
darwindel <darwindel00@gmail.com>
2021-07-01 23:08:04 -0400

    added diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*
+* print_diagonal - will print \ diagonally
+*
+* @n: will go into print_diagonal
+*
+* Return: 0 if successful
+*
+*/
+void print_diagonal(int n)
+{	
+
+	if (n <= 0)
+
+	_putchar('\\');
+	_putchar('\n');
+}
