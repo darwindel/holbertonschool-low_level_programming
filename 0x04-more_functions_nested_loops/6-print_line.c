b19ede2d18a64a34e9f1d5fc57814965cc988210
darwindel <darwindel00@gmail.com>
2021-07-01 22:49:28 -0400

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* print_line (int n);
+*
+* @n: will go in to print_line
+*
+* Return: 0 if successful
+*/
+void print_line(int n)
+{
+
+	if  (n < 10)
+{
+	_putchar('-');
+}
+	_putchar('\n');
+}
