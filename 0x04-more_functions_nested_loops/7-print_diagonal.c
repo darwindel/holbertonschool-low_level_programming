777d358e2cb9efdbf215a6e6e7bcc93004e787eb
darwindel <darwindel00@gmail.com>
2021-07-02 14:32:59 -0400

    fixed the code

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,18 +1,34 @@
 #include "holberton.h"
 /**
-*
-* print_diagonal - will print \ diagonally
+* print_diagonal - will print diagonally
 *
 * @n: will go into print_diagonal
 *
 * Return: 0 if successful
-*
 */
 void print_diagonal(int n)
-{	
+{
+	int a = 0;
+	int b = 0;
 
-	if (n <= 0)
+	if (n > 0)
+{
+	while (a < n)
+{
 
+	while (b < a)
+{
+	_putchar(' ');
+	b++;
+}
+	a++;
+	b = 0;
 	_putchar('\\');
 	_putchar('\n');
+}
+}
+	else
+
+	_putchar('\n');
+
 }
