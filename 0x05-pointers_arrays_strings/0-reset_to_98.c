2d61b24372788539eb954a5281871a46f43f1d04
darwindel <darwindel00@gmail.com>
2021-07-05 16:32:21 -0400

    pointing to pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* reset_to_98 - function that will take a pointer to an int
+*
+* @*n: this will go into the function
+*
+* Return: 0 if successful
+*/
+void reset_to_98(int *n)
+{
+	int *n
+
+	n = 98;
+}
