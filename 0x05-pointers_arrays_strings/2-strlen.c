fb99276c78ed2df1dac361f59a66c9f95c4aed24
darwindel <darwindel00@gmail.com>
2021-07-05 18:40:30 -0400

    will print length of str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* _strlen - will return the string length
+*
+* @s: this will go into the prototype
+*
+* Return: 0 if successful
+*
+*/
+int _strlen(char *s)
+{
+	for (s = 0; s != (void *)0; ++s)
+
+	printf("Length of Str is %s", s);
+
+	return (0);
+}
