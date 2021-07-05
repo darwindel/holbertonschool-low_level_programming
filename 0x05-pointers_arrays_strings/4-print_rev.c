44c6288da8139528ba52d7bed8fb7599d84525e2
darwindel <darwindel00@gmail.com>
2021-07-05 19:21:26 -0400

    will print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_rev - will print a string in reverse
+*
+* @s: will go into the prototype
+*
+* Return: 0 if successful
+*/
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = '\0'; s[i] != 0; i--)
+{
+	_putchar(s[i]);
+}
+	_putchar('\n');
+}
