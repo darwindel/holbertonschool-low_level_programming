605d883561aec4b44c0bca2f7d3840e44ea85838
darwindel <darwindel00@gmail.com>
2021-07-06 16:45:28 -0400

    final code

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,16 +12,27 @@
 void puts2(char *str)
 {
 
-	int i;
+	int i, a;
 	char l;
 
 	for (i = 0; str[i] != 0; i++)
-{
+	{
+	}
 	if (i % 2 == 0)
-
-	l = str[i];
+{
+	for ((a = i / 2); str[a] != 0; a++)
+{
+	l = str[a];
 	_putchar(l);
 }
+}
+	else
+{
+	for ((a = (i - 1) / 2); str[a] != 0; a++)
+{
+	l = str[a];
+	_putchar(l);
+}
+}
 	_putchar('\n');
-
 }
