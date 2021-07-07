9c9171d459467c6f1860841b0dfd6e4b21fa042f
darwindel <darwindel00@gmail.com>
2021-07-06 23:34:17 -0400

    print copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strcpy - this will copy the string pointed
+*
+* @dest: will go into prototype
+*
+* @src: will go into prototype
+*
+* Return: 0 if successful
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i, n;
+	int size = 0;
+
+	for (i = 0; i != '\0'; i++)
+{
+	size++;
+}
+	for (n = 0; n < size; n++)
+{
+	dest[i] = src[i];
+}
+	dest[size] = '\0';
+}
