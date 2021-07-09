a92875628c7a73a6681a83d1dea2504807e408d1
darwindel <darwindel00@gmail.com>
2021-07-08 21:34:24 -0400

    will copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* _strncpy - will copy a string
+*
+* @dest: will go into function
+* @src: will go into function
+* @n: will go into function
+*
+* Return: Always 0
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	for (n = 0; dest[n] != '\0'; n++)
+{
+	src[n] = dest[n];
+}
+	src[n] = '\0';
+}	
