4642e0e66fe43ec74edb4beedf022535e2e09f5b
darwindel <darwindel00@gmail.com>
2021-07-09 12:43:20 -0400

    will print caps

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* string_toupper - will convert lowercase to uppercase in string
+*
+* @: will go into function
+*
+* Return: 0 if successful
+*/
+char *string_toupper(char *s)
+{
+	int i;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+{
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+	s[i] =   s[i] - 32;
+	}
+}
+{
+	return (s);
+}
+}
