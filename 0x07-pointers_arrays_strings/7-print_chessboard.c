9b6fa360306fde274711260618507e9be62746b2
darwindel <darwindel00@gmail.com>
2021-07-10 15:51:02 -0400

    prints chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_chessboard - will print the chessboard
+*
+* @a: row
+*
+* Return: 0 if successful
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, x;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (x = 0; x < 8; x++)
+		{
+		if (x == 7)
+		{
+		_putchar(a[i][x]);
+		_putchar('\n');
+		}
+		else
+		_putchar(a[i][x]);
+		}
+	}	
+}
