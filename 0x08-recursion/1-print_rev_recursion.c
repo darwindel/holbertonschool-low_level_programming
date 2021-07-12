1bb4b0bf9a9c16bcb091c84844fb2ffc1a4d33d6
darwindel <darwindel00@gmail.com>
2021-07-12 19:18:48 -0400

    will reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _print_rev_recursion - will print string in reverse
+*
+* @s: will be the string
+*
+* Return: nothing
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{	
+	_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
