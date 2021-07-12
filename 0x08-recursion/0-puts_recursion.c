afa61eff435b653869a88c58967860c38a44c875
darwindel <darwindel00@gmail.com>
2021-07-12 18:56:19 -0400

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _puts_recursion - will print a string followed by a new line
+*
+* @s: the string
+*
+* Return: 0 if successful
+*/
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		_putchar('\n');	
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}	
