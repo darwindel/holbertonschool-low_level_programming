d62a5ea79e2f51cc1d49a2ee328b8381b138acdf
darwindel <darwindel00@gmail.com>
2021-07-12 23:09:33 -0400

    will return square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* _sqrt_recursion - will return square root of a number
+*
+* @n: number that will be tested for square root
+*
+* Return: 0 if successful
+*/
+int _sqrt_recursion(int n)
+{
+
+
+	if ((n * n) == n)
+	{	
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+
+	return (e_sqrt_recursion(--n));
+	}	
+}
