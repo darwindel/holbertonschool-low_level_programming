5d3853affcab8bdcff7598c4fb36a77908cb73d6
darwindel <darwindel00@gmail.com>
2021-07-12 20:14:30 -0400

    fixed order of the code

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,14 +8,12 @@
 */
 int _strlen_recursion(char *s)
 {
-	{
-	static int l = 1;
 
-	if (*s != '\0')
+	if (*s != 0)
 		{
-		l++;
-		_strlen_recursion(s + 1);
+
+		return (1 + _strlen_recursion(++s));
+
 		}
-		return (l);
-	}
+	return (0);
 }
