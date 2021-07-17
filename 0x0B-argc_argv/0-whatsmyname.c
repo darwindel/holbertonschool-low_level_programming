2e223368b721054ac797a32059823cd15d1bd8cc
darwindel <darwindel00@gmail.com>
2021-07-16 21:29:09 -0400

    will print new line

---
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - program that prints its name, followed by a new line
+*
+* @argc: argument count
+* @argv: argument vector
+*
+* Return 0: if successful
+*/
+int main (int argc, char *argv[])
+{
+	argc(void);
+
+	printf("%s\n", arg[0]);
+
+	return (0);
+}
