f0d27fdb90b66cf60c6a91d785ef9fd0318f819f
darwindel <darwindel00@gmail.com>
2021-07-17 13:50:44 -0400

    adding two numbers

---
+++ 0x0B-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - add positive numbers
+*
+* @argc: number
+* @argv: vector
+*
+* Return: 0 if successful
+*/
+int main(int argc, char *argv[])
+{
+	int c, d;
+	int n = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (c = 1; c < argc; c++)
+	{
+		for (d = 0; argv[c][d] != '\0'; d++)
+		{
+			if (argv[c][d] < '0' || argv[c][d] > '9')
+			{
+			printf("Error\n");
+			return (1);
+			}
+		}
+		n += atoi(argv[c]);
+	}
+	printf("%d\n", n);
+	return (0);
+}
