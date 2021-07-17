8310a1822534f42a62e266f5d4fc0f0f40189247
darwindel <darwindel00@gmail.com>
2021-07-16 21:50:29 -0400

    print the number of arg

---
+++ 0x0B-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - program that prints the number of arguments passed into it
+*
+* @argc: number
+* @argc: vector
+*
+* Return: 0 if successful
+*/
+int main(int argc, char* argv[])
+{
+	print("argc = %d\n", argc);
+	return (0);
+}
