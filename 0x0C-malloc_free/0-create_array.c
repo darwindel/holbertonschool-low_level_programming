572c4d2f4b83b8c9b8aeb6e411d7fe2b95c63ba9
darwindel <darwindel00@gmail.com>
2021-07-20 22:14:13 -0400

    function

---
+++ 0x0C-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* create_array - will create array of chars and initialize with specific char
+*
+* @size: size of array
+* @c: specific char
+*
+* Return: 0 if successful
+*
+*/
+char *create_array(unsigned int size, char c)
+{
+
+
+
+
+
+
+}
