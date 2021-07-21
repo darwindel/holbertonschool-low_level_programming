1b36817a14d59d2281189a5d9c8502ab32cb62f0
darwindel <darwindel00@gmail.com>
2021-07-20 22:12:07 -0400

    prototypes

--- 0x0C-malloc_free/holberton.h
+++ 0x0C-malloc_free/holberton.h
@@ -1,6 +1,10 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
+<stdio.h>
+<stdlib.h>
+
+
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
