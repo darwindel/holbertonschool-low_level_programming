673fc9209d9e49e8b328116d81ab6ee952823b7e
darwindel <darwindel00@gmail.com>
2021-07-20 22:30:51 -0400

    final

--- 0x0C-malloc_free/0-create_array.c
+++ 0x0C-malloc_free/0-create_array.c
@@ -8,23 +8,22 @@
 * @c: specific char
 *
 * Return: pointer to array, or NULL if it fails
-*
 */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *str;
 
-	str = malloc (sizeof(char) * size);
+	str = malloc(sizeof(char) * size);
 
 	if (size == 0 || str == NULL)
-		return (NULL);	
+	return (NULL);
 
 	i = 0;
-	while(i < size)
+	while (i < size)
 	{
 		str[i] = c;
 		i++;
 	}
-	return(str);
+	return (str);
 }
