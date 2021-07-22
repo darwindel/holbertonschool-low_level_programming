69c0e611552d7b37ab8f3e05d42f5cdcb12ca8a4
darwindel <darwindel00@gmail.com>
2021-07-21 21:19:43 -0400

    print name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - will print name of file
+*
+* @argc: count
+* @argv: array vector
+* Return: name of file
+*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s", argv[0]);
+	return (0);
+}
