5bb1cb84f2a7e486c1622252bbc2f697480a5691
darwindel <darwindel00@gmail.com>
2021-07-23 15:41:26 -0400

    concat

--- 0x0E-more_malloc_free/1-string_nconcat.c
+++ 0x0E-more_malloc_free/1-string_nconcat.c
@@ -14,15 +14,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
 	unsigned int i, j;
+	int s1len = 0;
+	int s2len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	while (s1[s1len])
+		s1len++;
+	while (s2[s2len])
+		s2len++;
+
 	for (i = 0; s1[i]; i++)
 
-	s3 = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*s3)));
+	s3 = malloc(sizeof(char) * (s1len + s2len + 1));
 
 	if (s3 == NULL)
 		return (NULL);
