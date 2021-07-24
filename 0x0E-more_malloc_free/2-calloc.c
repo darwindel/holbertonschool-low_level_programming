e5fbf3abed66e13cfbfbe70b806366afd8f366c6
darwindel <darwindel00@gmail.com>
2021-07-23 21:08:15 -0400

    final

---
+++ 0x0E-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* _calloc - allocates mem for array
+*
+* @nmemb: numb of elements
+* @size: size of element type
+*
+* Return: a pointer to allocated mem, NULL if nmemb or size is 0
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ar;
+	unsigned int i;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	ar = malloc(size * nmemb);
+	if (ar == NULL);
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		ar[i] = 0;
+
+	return (ar);
+}
