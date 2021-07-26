cf4c351c197ea40bf603e3874c6fc818d5b9134f
darwindel <darwindel00@gmail.com>
2021-07-26 15:52:45 -0400

    new struct type

---
+++ 0x0F-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog - describes the dog
+*
+* @name: name of dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
