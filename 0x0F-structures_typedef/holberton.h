a1356342a3a3a87b3915794b4650508b625fcc90
darwindel <darwindel00@gmail.com>
2021-07-26 15:33:28 -0400

    prototypes

---
+++ 0x0F-structures_typedef/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
