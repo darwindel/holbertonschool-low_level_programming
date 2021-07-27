49d6fc8ebb5d33d8f6ea97c185eaa9700f5722e3
darwindel <darwindel00@gmail.com>
2021-07-26 22:13:50 -0400

    new dog

---
+++ 0x0F-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* new_dog - create a new dog
+*
+* @name: name of new dog
+* @age: age of new dog
+* @owner: owner of new dog
+* Return: nothing
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
