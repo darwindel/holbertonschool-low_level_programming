732b08945dcb26332720b7557bc5c7dee67aa40a
darwindel <darwindel00@gmail.com>
2021-08-11 18:14:08 -0400

    fixed betty errors

--- 0x14-more_singly_linked_lists/lists.h
+++ 0x14-more_singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
-#ifndef _LISTS_H_INCLUDED
-#define _LISTS_H_INCLUDED
-
+#ifndef _LISTS_H_
+#define _LISTS_H_
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,8 +11,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
@@ -26,5 +26,4 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
 #endif
