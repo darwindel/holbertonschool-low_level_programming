3427273d7414b3078bb5f63bc5db27fe5f375159
darwindel <darwindel00@gmail.com>
2021-08-11 17:46:25 -0400

    will print

---
+++ 0x14-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+* print_listint - prints all the elements of a listint_t list
+*
+* @h: pointed to the list
+*
+* Return: the elements of the list
+*/
+size_t print_listint(const listint_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+	printf("[%i]", h->n);
+
+	h = h->next;
+	counter++;
+	}
+	return (counter);
+}
