bdb5146f6b18c0de5fff333fe84fb63e472cb5c4
darwindel <darwindel00@gmail.com>
2021-08-11 17:49:32 -0400

    will print

--- 0x14-more_singly_linked_lists/0-print_listint.c
+++ 0x14-more_singly_linked_lists/0-print_listint.c
@@ -13,10 +13,10 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-	printf("[%i]", h->n);
+		printf("%i\n", h->n);
 
-	h = h->next;
-	counter++;
+		h = h->next;
+		counter++;
 	}
 	return (counter);
 }
