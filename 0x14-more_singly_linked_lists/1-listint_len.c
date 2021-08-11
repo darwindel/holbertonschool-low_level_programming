46888626bf602129f8cb0c47b27ef2b06d8e791c
darwindel <darwindel00@gmail.com>
2021-08-11 18:29:32 -0400

    list

---
+++ 0x14-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+* listint_len - return the number of elements in a linked listint_t
+*
+* @h: linked list
+*
+* Return: the number of elements in h
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+	h = h->next;
+	counter++;
+	}
+		return (counter);
+{
