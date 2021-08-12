580f9fcad80e424b50bcf00cede3afb0ecc9d229
darwindel <darwindel00@gmail.com>
2021-08-11 23:53:53 -0400

    adding file

---
+++ 0x14-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* add_nodeint_end - adds a new node at the end of a list
+*
+* @head: the head of list
+* @n: the data to add
+*
+* Return: the address of the new element, NULL if failed
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+
+
+
+}
