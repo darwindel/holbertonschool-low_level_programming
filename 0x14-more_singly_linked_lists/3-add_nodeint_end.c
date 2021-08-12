fa1e1d52b357f33b8ffb92b842b988d70efe40bd
darwindel <darwindel00@gmail.com>
2021-08-12 00:18:25 -0400

    add node at end

--- 0x14-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x14-more_singly_linked_lists/3-add_nodeint_end.c
@@ -31,9 +31,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	temp = *head;
 	while (temp->next)
 		temp = temp->next;
-	temp->next = new
+	temp->next = new;
 	}
 	return (new);
-}
-
 }
