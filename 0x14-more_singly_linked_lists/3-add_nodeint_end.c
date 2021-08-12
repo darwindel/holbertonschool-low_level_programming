ea3599ff488437c1524f410b9eb31207612a9032
darwindel <darwindel00@gmail.com>
2021-08-12 00:16:44 -0400

    add to end

--- 0x14-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x14-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,30 @@
 */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *new;
+	listint_t *temp;
 
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+	*head = new;
+	return (new);
+	}
+
+	else
+	{
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new
+	}
+	return (new);
+}
 
 }
