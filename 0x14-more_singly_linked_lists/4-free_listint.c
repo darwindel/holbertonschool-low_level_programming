c063f08fedfae2e3e905f4f8ef1e51d54b87c408
darwindel <darwindel00@gmail.com>
2021-08-11 20:10:57 -0400

    free list

---
+++ 0x14-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/*
+* free_listint - will free the listint_t list
+*
+* @head: beginning of the list to free
+* Return: nothing
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
