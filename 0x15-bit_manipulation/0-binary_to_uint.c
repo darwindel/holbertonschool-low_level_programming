fe11ba2078a4e5590ffd75f836887edb1df090f2
darwindel <darwindel00@gmail.com>
2021-08-13 14:46:57 -0400

    final

--- 0x15-bit_manipulation/0-binary_to_uint.c
+++ 0x15-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
 * binary_to_uint - will convert binary to unsigned int
@@ -8,20 +9,17 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int L, end = 0, B = 1;
+	unsigned int L = 0;
 	int i;
 
 	if (b == NULL)
 		return (0);
-	for (L = 0; b[L] != '\0'; L++)
-		;
-	for (i = L - 1; i >= 0; i--)
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' && b[i] > '1')
 			return (0);
-		if (b[i] == '1')
-			end += B;
-			B = B * 2;
+		L = 2 * L + (b[i] - '0');
 	}
-		return (end);
+		return (L);
 }
