ace54c51eede7e485772b7b8137a93cb3ad13fe8
darwindel <darwindel00@gmail.com>
2021-08-18 10:10:54 -0400

    made changes

--- 0x16-file_io/1-create_file.c
+++ 0x16-file_io/1-create_file.c
@@ -15,23 +15,23 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 		while (text_content[length])
-			length++
-	x = open(filename, O_WRONLY | O_TRUNC);
-	if (x < 0)
+			length++;
+	n = open(filename, O_WRONLY | O_TRUNC);
+	if (n < 0)
 	{
-		x = open(filename, O_WRONLY | O_CREAT, 0600);
-		if (x < 0)
+		n = open(filename, O_WRONLY | O_CREAT, 0600);
+		if (n < 0)
 			return (-1);
-		result = write(x, text_content, length);
+		result = write(n, text_content, length);
 		if (result < 0)
 			return (-1);
 	}
 	else
 	{
-		result = write(x, text_content, length);
+		result = write(n, text_content, length);
 		if (result < 0)
 			return (-1);
 	}
-	close(x);
+	close(n);
 	return (1);
 }
